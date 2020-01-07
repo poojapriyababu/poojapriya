7fdc2e1154e6615476a93e7ce6de09c38b40f9d7
poojapriyababu <52281066+poojapriyababu@users.noreply.github.com>
2020-01-07 10:40:15 +0530

    Create weeknum.py

---
+++ weeknum.py
@@ -0,0 +1,18 @@
+weeknum=input("enter the week number")
+weeknum=int(weeknum)
+if(weeknum==1):
+  print("sunday")
+elif(weeknum==2):
+  print("monday")
+elif(weeknum==3):
+  print("tuseday")
+elif(weeknum==4):
+  print("wednesday")
+elif(weeknum==5):
+  print("thursday")
+elif(weeknum==6):
+  print("friday")
+elif(weeknum==7):
+  print("saturday")
+else:
+  print("please enter the weeknum")      
